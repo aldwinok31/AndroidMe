7542fdc11a3814a8885bfbbd4f9cc2f09b761019
Bobby <aldwinotablante@gmail.com>
2017-09-08 21:29:38 +0800

    Initial

--- app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
+++ app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
@@ -2,7 +2,9 @@ package com.hyperdev.androidme.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.hyperdev.androidme.BodyPartFragment;
 import com.hyperdev.androidme.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BodyPartFragment bodyPartFragment = new BodyPartFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.BodyPartFragmentContainer, bodyPartFragment)
+                .commit();
     }
+
 }
